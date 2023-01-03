ae79d0b7b2c763c923326be8e8432e23de7d07f5
OlhaDorofiychuk <olha.d@yahoo..com>
2023-01-03 18:21:07 +0000

    in progress

--- src/movie.js
+++ src/movie.js
@@ -1,37 +1,40 @@
+import { useEffect,useState } from "react";
 export const Movie = ({ selectedMovie, favoriteClicked, handleClick }) => {
-  return {
-    if(selectedMovie) {
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${selectedMovie.movie_id}?api_key=6b2aabd11953836de38f90530f997962`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMovie(data.results);
+      });
+  }, [selectedMovie]);
+  return (
       <div className="movie">
-        <h2> {selectedMovie.title}</h2>
-        <span className="age">
-          Age Restrigtion:<b>{selectedMovie.adult === false ? "No" : "Yes"}</b>
-        </span>
-        <img
-          src={`https://image.tmdb.org/t/p/w200${selectedMovie.poster_path}`}
-          alt={selectedMovie.title}
-        />
-        <span className="reactions">
-          <span
-            onClick={handleClick}
-            className={
-              favoriteClicked === true
+        <><h2> {movie.title}</h2><span className="age">
+          Age Restrigtion:<b>{movie.adult === false ? "No" : "Yes"}</b>
+        </span><img
+            src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+            alt={movie.title} /><span className="reactions">
+            <span
+              onClick={handleClick}
+              className={favoriteClicked === true
                 ? "material-symbols-outlined favorite"
-                : "material-symbols-outlined"
-            }
-          >
-            favorite
-          </span>
-          <span className="material-symbols-outlined check">check</span>
-          <span className="material-symbols-outlined">close</span>
-        </span>
-        <span className="info">Overview: {selectedMovie.overview} </span>
-        <span className="genreId">Genre ID: {selectedMovie.genre_ids}</span>
-        <span className="score">
-          Popularity Score:
-          {selectedMovie.popularity}
-        </span>
-        <span className="date">Release Date:{selectedMovie.release_date}</span>
-      </div>;
-    },
+                : "material-symbols-outlined"}
+            >
+              favorite
+            </span>
+            <span className="material-symbols-outlined check">check</span>
+            <span className="material-symbols-outlined">close</span>
+          </span><span className="info">Overview: {movie.overview} </span><span className="genreId">Genre ID: {movie.genre_ids}</span><span className="score">
+            Popularity Score:
+            {movie.popularity}
+          </span><span className="date">Release Date:{movie.release_date}</span></>
+      </div>
+  )
   };
-};
+
